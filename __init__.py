c6f12e128b100ad6631eeaae2ff1b622bbf7ec23
zillab <107042060+ZILLABB@users.noreply.github.com>
2025-05-27 12:47:44 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,19 @@
+app/
+├── __init__.py
+├── services/
+│   └── __init__.py
+├── ml/
+│   └── __init__.py
+└── utils/
+    └── __init__.py
 
+tests/
+├── __init__.py
+└── unit/
+    ├── __init__.py
+    ├── ml/
+    │   └── __init__.py
+    ├── services/
+    │   └── __init__.py
+    └── utils/
+        └── __init__.py
